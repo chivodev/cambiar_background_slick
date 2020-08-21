a08930be77ea7431b22724d3d0f1c8f62dbde5ca
chivodev <chivodev@chivodev.com>
2020-08-21 16:37:16 -0500

    Se mejora la transicion y cambio del fondo

--- slider.js
+++ slider.js
@@ -2,13 +2,18 @@ $(document).ready(function () {
 	$('.slider-cambiar').slick();
 })
 
-$('.slider-cambiar').on('afterChange', function(slick, currentSlide){
-	var idElemento = $(slick.currentTarget).children('.slick-list').find('.slick-active').children('img').attr('id');
-	cambiar_background(idElemento,'containerSlick');
-});
+// $('.slider-cambiar').on('afterChange', function(slick, currentSlide){
+// 	var idElemento = $(slick.currentTarget).children('.slick-list').find('.slick-active').children('img').attr('id');
+// 	cambiar_background(idElemento,'containerSlick');
+// });
 
 // On init
 $('.slider-cambiar').on('init', function(slick){
 	var idElemento = $(slick.currentTarget).children('.slick-list').find('.slick-active').children('img').attr('id');
+	cambiar_background(idElemento,'containerSlick');
+});
+
+$('.slider-cambiar').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+	var idElemento = $(slick.$slides[nextSlide]).children('img').attr('id');
 	cambiar_background(idElemento,'containerSlick');
 });
